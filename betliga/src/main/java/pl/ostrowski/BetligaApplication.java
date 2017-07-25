4f5571b8916ff6dbe5c7c09361c599fa3f0c0a8d
Jędrzej Ostrowski <jedrzej.ostrowski@gfactor.pl>
2017-07-25 15:06:27 +0200

    Dodałem initial project

---
+++ betliga/src/main/java/pl/ostrowski/BetligaApplication.java
@@ -0,0 +1,12 @@
+package pl.ostrowski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetligaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetligaApplication.class, args);
+	}
+}
